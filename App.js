048d1b0fa9be96155ba1b06bc7ce6ccb8850d86f
liukefu2050 <liukefu2050@sina.com>
2020-07-07 15:55:05 +0800

    2

--- App.js
+++ App.js
@@ -1,114 +1,212 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
+import React, {Component} from 'react';
 import {
+  Appearance, StyleSheet, Text, TouchableOpacity, View, KeyboardAvoidingView,
   SafeAreaView,
-  StyleSheet,
+  InteractionManager,
   ScrollView,
-  View,
-  Text,
-  StatusBar,
+  Alert
 } from 'react-native';
+import {RichEditor, RichToolbar} from 'react-native-editor';
+import ImagePicker from "react-native-customized-image-picker";
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const initHTML = `<br/>
+<center><b>Pell.js Rich Editor</b></center>
+<center>React Native</center>
+<br/>
+<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1024px-React-icon.svg.png" ></br></br>
+</br></br>
+`;
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
+export default class RichTextExample extends Component {
+
+  constructor(props) {
+    super(props);
+    const that = this;
+    const theme = props.theme || Appearance.getColorScheme();
+    const contentStyle = that.createContentStyle(theme);
+    that.state = {theme: theme, contentStyle};
+  }
+
+  onPressInsert = () => {
+    this.richText.setContentHTML("<p>insert by setContentHTML</p>");
+  }
+
+  onPressAppendInsert = () => {
+    this.richText.appendContentHTML("<p>append by appendContentHTML</p>");
+  }
+
+  onPressGotFocus = () => {
+    this.richText.focusContentEditor();
+  }
+  multiPost = (files, successCallback, failCallback) => {
+    let url = "https://www.xiushangsh.com/upload.json";
+    let formData = new FormData();
+    for(let i=0;files&&i<files.length;i++){
+      let obj = files[i];
+      if(!obj.type){
+        obj.type = 'multipart/form-data';
+      }
+      if(!obj.name){
+        let tmp = obj.uri;
+        if(tmp){
+          let index = tmp.lastIndexOf("/");
+          if(index!= -1){
+            obj.name = tmp.substr(index+1);
+          }
+        }else{
+          obj.name = 'image'+i+".jpg";
+        }
+      }
+      if(obj.key){
+        formData.append(obj.key,obj);
+      }else {
+        formData.append("images"+i,obj);
+      }
+    }
+
+    let fetchOptions = {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'multipart/form-data',
+      },
+      body: formData
+    };
+
+    fetch(url, fetchOptions)
+        .then((response) => response.text())
+        .then((responseText) => {
+          let result = JSON.parse(responseText);
+          successCallback(result);
+        })
+        .catch((err) => {
+          console.log(err);
+          failCallback(err);
+        });
+  }
+
+  pickForEdit = () => {
+    let _this = this;
+
+    ImagePicker.openPicker({
+      isCamera: true,
+      compressQuality: 90
+    }).then(images => {
+      _this.richText.appendContentHTML("<p>openPicker:"+JSON.stringify(images)+"</p>");
+      if (images && images.length) {
+        let imageList = [];
+        imageList.push(images[0]);
+        _this.multiPost(imageList, function (uploadImages) {
+          _this.richText.appendContentHTML("<p>successCallback:"+JSON.stringify(uploadImages)+"</p>");
+          let imageList = ["https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1024px-React-icon.svg.png"];
+
+          _this.richText.insertImage({src: imageList[0]});
+          _this.richText.blurContentEditor();
+        },function (error) {
+          _this.richText.appendContentHTML("<p>failCallback:"+JSON.stringify(error)+"</p>");
+        })
+      }
+    }).catch(e => {
+      _this.richText.appendContentHTML("<p>openPicker catch:"+JSON.stringify(e)+"</p>");
+    });
+  }
+
+
+  createContentStyle (theme){
+    const contentStyle = {backgroundColor: '#000033', color: '#fff', placeholderColor: 'gray'};
+    if (theme === 'light') {
+      contentStyle.backgroundColor = '#fff';
+      contentStyle.color = '#000033';
+      contentStyle.placeholderColor = '#a9a9a9';
+    }
+    return contentStyle;
+  }
+
+  async onPressGotValue() {
+    // Get the data here and call the interface to save the data
+    let html = await this.richText.getContentHtml();
+    // console.log(html);
+    alert(html);
+  }
+
+  render() {
+    let that = this;
+    const {contentStyle, theme} = that.state;
+    const {backgroundColor, color, placeholderColor} = contentStyle;
+    const themeBg = {backgroundColor};
+    return (
+        <SafeAreaView style={styles.container}>
+          <View style={{flexDirection: 'row', height: 50, alignItems: 'center',justifyContent: 'center', backgroundColor: '#847b66'}}>
+            <TouchableOpacity style={{marginLeft: 10, padding: 5}} onPress={()=>that.pickForEdit()}>
+              <Text>upload</Text>
+            </TouchableOpacity>
           </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+          <ScrollView style={[styles.scroll, themeBg]} keyboardDismissMode={'none'}>
+            <RichEditor
+                editorStyle={contentStyle}
+                containerStyle={themeBg}
+                ref={(r) => that.richText = r}
+                style={[styles.rich, themeBg]}
+                placeholder={'please input content'}
+                initialContentHTML={initHTML}
+            />
+          </ScrollView>
+          <KeyboardAvoidingView behavior={'padding'}>
+            <RichToolbar
+                style={[styles.richBar, themeBg]}
+                getEditor={() => that.richText}
+                iconTint={color}
+                selectedIconTint={'#2095F2'}
+                selectedButtonStyle={{backgroundColor: 'transparent'}}
+                onPressAddImage={that.pickForEdit}
+            />
+          </KeyboardAvoidingView>
+
+        </SafeAreaView>
+    );
+  }
+
+  onEditorInitialized() {
+    this.setFocusHandlers();
+    this.getHTML();
+  }
+
+  async getHTML() {
+    const titleHtml = await this.richText.getTitleHtml();
+    const contentHtml = await this.richText.getContentHtml();
+    //alert(titleHtml + ' ' + contentHtml)
+  }
+
+  setFocusHandlers() {
+    this.richText.setTitleFocusHandler(() => {
+      //alert('title focus');
+    });
+    this.richText.setContentFocusHandler(() => {
+      //alert('content focus');
+    });
+  }
+}
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  container: {
+    flex: 1,
+    backgroundColor: '#F5FCFF',
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  rich: {
+    minHeight: 300,
+    flex: 1,
   },
-  body: {
-    backgroundColor: Colors.white,
+  richBar: {
+    height: 50,
+    bottom:0,
+    backgroundColor: '#F5FCFF',
   },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  scroll: {
+    flex: 1,
+    backgroundColor: '#ffffff',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  input: {
+    flex: 1,
   },
 });
 
-export default App;
